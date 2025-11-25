80e4733b9409529590865063554f6620a5dd42e8
Aleks Rutins <keeper@farthergate.com>
2025-11-25 13:02:54 -0500

    Better rendering logic, support text nodes

--- src/render-element.lisp
+++ src/render-element.lisp
@@ -4,19 +4,27 @@
   (concatenate 'string
                (symbol-name name) "=\"" val "\" "))
 
+(defun render-children (children)
+  (apply #'concatenate 'string
+         (mapcar (lambda (c)
+                   (if (stringp c)
+                       c
+                       (render-element c)))
+                 children)))
+
 (defun render-element (el)
   (let* ((name (car el))
          (properties (cdr el))
          (children (getf properties :children)))
     (concatenate 'string
-                 "<" (symbol-name name) " "
-                 (apply #'concatenate 'string
-                        (loop for (name val) on properties by #'cddr
-                              if (not (eq name :children))
-                              collect (render-attribute name val)))
+                 "<" 
+                 (string-trim " " 
+                              (concatenate 'string
+                                           (symbol-name name) " "
+                                           (apply #'concatenate 'string
+                                                  (loop for (name val) on properties by #'cddr
+                                                        if (not (eq name :children))
+                                                        collect (render-attribute name val)))))
                  ">"
-                 (if children
-                     (apply #'concatenate 'string 
-                            (mapcar #'render-element children))
-                     "")
+                 (render-children children)
                  "</" (symbol-name name) ">")))
